c5c5dee3518548dd1c4c896b9387a640770dbf35
Abrar <abrar.ksu@gmail.com>
2020-05-27 08:10:51 +0300

    Initial Commit

---
+++ Image Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Picker
+//
+//  Created by imac on 5/27/20.
+//  Copyright © 2020 Abrar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
