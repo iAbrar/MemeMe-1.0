8eb6d3de23a7b22a93bb578f70c91afabcbe3b97
Abrar <abrar.ksu@gmail.com>
2020-05-29 17:12:02 +0300

    fix: Issue of black screen when user typed in bottom text

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -44,9 +44,21 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         //Disabling the camera button in simulator
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
 
-        // Set the delegate
+        // Set the delegates
         self.topText.delegate = self
          self.bottomText.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     // Hide time and battary in the top 
@@ -116,5 +128,39 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         
         return true;
     }
+    
+    //Fix issue of disapearing keyboard
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if self.bottomText.isFirstResponder {
+        view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
 }
 
