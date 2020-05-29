0f6ae6e46271eecaa6cbd2ed4b5e7c1c814824ad
Abrar <abrar.ksu@gmail.com>
2020-05-29 09:04:56 +0300

    feat: Reset text and dismiss keyboard

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -9,19 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
-
+UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    
+    // MARK: Properties
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  5
+    ]
+    
     // Outlets
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
     
 // Text Field Delegate objects
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-       
+        // Set text for each field
+        self.topText.text = "TOP"
+        self.topText.defaultTextAttributes = memeTextAttributes
+        self.topText.textAlignment = .center
+        
+        self.bottomText.text = "BOTTOM"
+        self.bottomText.defaultTextAttributes = memeTextAttributes
+        self.bottomText.textAlignment = .center
+        
+        //Disabling the camera button in simulator
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+        // Set the delegate
+        self.topText.delegate = self
+         self.bottomText.delegate = self
     }
     
     // Hide time and battary in the top 
@@ -60,5 +85,36 @@ UINavigationControllerDelegate {
         dismiss(animated: true, completion: nil)
     }
     
+   
+    
+    // MARK: Text Field Delegate Methods
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//        // Figure out what the new text will be, if we return true
+//        var newText = textField.text! as NSString
+//        newText = newText.replacingCharacters(in: range, with: string) as NSString
+//
+//
+//        self.topText.text = String(newText.length)
+//
+//        // returning true gives the text field permission to change its text
+//        return true;
+//    }
+    
+    //Remove default text
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if(textField.text == "TOP" || textField.text == "BOTTOM")
+        {
+             textField.text = ""
+        }
+    }
+    
+    //keyboard dismissed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
 }
 
